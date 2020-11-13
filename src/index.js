bfbe9ebd491609efd3c1b5c0f58ef466893ce4d1
Varun <varunsridharan23@gmail.com>
2020-11-13 12:09:29 +0530

    Updated
    Signed-off-by: Varun <varunsridharan23@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = {
 	log: require( './logger/index' ),
 	exec: require( './node-exec' ),
 	git: require( './git/index' ),
-	path: require( './path' ),
-	asyncForEach: require( './async-foreach' ),
-	input: require( './inputs' ),
+	path: require( './path/index' ),
+	asyncForEach: require( './asyncForEach/index' ),
+	input: require( './input/index' ),
 };
