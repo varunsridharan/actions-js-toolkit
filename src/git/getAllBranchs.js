6b819381cba0d6d543c7cf90c7abf6fcdf1c71e1
Varun <varunsridharan23@gmail.com>
2020-11-14 07:07:04 +0530

    added option to fetch all branches name.
    Signed-off-by: Varun <varunsridharan23@gmail.com>

--- src/git/getAllBranchs.js
+++ src/git/getAllBranchs.js
@@ -2,7 +2,7 @@ const nodeexec = require( '../node-exec' );
 
 module.exports = async( work_dir ) => {
 	let status = true;
-	let cmd    = 'git branch --show-current';
+	let cmd    = 'git branch --no-color --list --no-column';
 	await nodeexec( `${cmd}`, work_dir ).then( ( response ) => status = response ).catch( () => status = false );
 	return status;
 };
--- src/git/index.js
+++ src/git/index.js
@@ -1,10 +1,11 @@
-const identity      = require( './identity' );
-const add           = require( './add' );
-const hasChange     = require( './has-change' );
-const stats         = require( './stats' );
-const commit        = require( './commit' );
-const push          = require( './push' );
-const currentBranch = require( './current-branch' );
+const identity       = require( './identity' );
+const add            = require( './add' );
+const hasChange      = require( './has-change' );
+const stats          = require( './stats' );
+const commit         = require( './commit' );
+const push           = require( './push' );
+const currentBranch  = require( './current-branch' );
+const getAllBranches = require( './getAllBranchs' );
 
-module.exports = { identity, add, stats, hasChange, commit, push, currentBranch };
+module.exports = { identity, add, stats, hasChange, commit, push, currentBranch, getAllBranches };
 
